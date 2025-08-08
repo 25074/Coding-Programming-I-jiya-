236ddbbf861232b60f38c98112ebcfbfc7909ccc
25074 <jiyap91011@gmail.com>
2025-08-08 22:13:55 +0530

    Add files via upload

---
+++ JIYA dollard into pounds.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+float a,b;
+printf("Enter amount in dollars to convert it into pounds");
+scanf("%f",&a);
+b=a*0.685;
+printf("%fdollars=%fpounds",a,b);
+
+}
---
+++ JIYA gm to kg.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+int a,b;
+printf("Enter value in grams to convert it into kilograms");
+scanf("%d",&a);
+b=a/1000;
+printf("%dgrams=%dkilograms",a,b);
+}
