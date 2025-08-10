01ac3405060c3b67b1138de50f6c022cd0415e69
25074 <jiyap91011@gmail.com>
2025-08-10 11:32:46 +0530

    Add files via upload

---
+++ JIYA area of circle.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+float r,a;
+printf("Enter the value of radius");
+scanf("%f",&r);
+a=3.14*r*r;
+printf("Area of circle=%f\n",a);
+
+}
---
+++ JIYA area of triangle.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main ()
+{
+int h,l,a;
+printf("Enter the values");
+scanf("%d %d",&h,&l);
+a=h*l/2;
+printf("Area of triangle=%d",a);
+}
