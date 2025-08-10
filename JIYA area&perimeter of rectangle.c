bd73edb191e696a7a46e20807f8c7f8bb5f158a3
25074 <jiyap91011@gmail.com>
2025-08-10 11:16:39 +0530

    Add files via upload

---
+++ JIYA area&perimeter of rectangle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int a,p,l,b;
+printf("Enter the value of length and breadth");
+scanf("%d %d",&l,&b);
+a=l*b;
+printf("Area=%d\n",a);
+p=2*(l+b);
+printf("Perimeter=%d\n",p);
+
+}
