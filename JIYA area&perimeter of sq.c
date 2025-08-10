a1b340811c0550e41b758cb5c6e01d0ff39469de
25074 <jiyap91011@gmail.com>
2025-08-10 11:09:37 +0530

    Add files via upload

---
+++ JIYA area&perimeter of sq.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int A,P,L;
+printf("Enter the length");
+scanf("%d",&L);
+A=L*L;
+printf("AREA=%d\n",A);
+P=4*L;
+printf("Perimeter=%d\n",P);
+
+}
---
+++ JIYA interest.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+int p,r,n,i;
+printf("Enter the values PRN:");
+scanf("%d %d %d",&p,&r,&n);
+i=(p*r*n)/100;
+printf("Interest=%d",i);
+}
+
+
