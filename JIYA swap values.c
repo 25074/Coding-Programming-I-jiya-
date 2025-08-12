e808ab0471b16dec56a53a796ac0eb8e1ca9942f
25074 <jiyap91011@gmail.com>
2025-08-12 21:28:15 +0530

    Add files via upload

---
+++ JIYA swap values.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+printf("Enter the value of a\n");
+scanf("%d",&a);
+printf("Enter the value of b\n");
+scanf("%d",&b);
+
+printf("a=%d\n",b);
+printf("b=%d\n",a);
+
+
+
+
+
+}
