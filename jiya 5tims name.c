b2f33f956db1cdf1d891970a5ae588c92536c994
25074 <jiyap91011@gmail.com>
2025-09-07 00:32:49 +0530

    Add files via upload

---
+++ jiya 5tims name.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+int a;
+for(a=1; a<=5; a=a+1)
+{
+    printf("Jiya\n");
+}
+}
---
+++ jiya ntimes name.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+int a,n;
+printf("Enter a value\n");
+scanf("%d",&n);
+for(a=1; a<=n; a=a+1)
+{
+    printf("Jiya\n");
+}
+}
