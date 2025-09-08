ddcf605257decb84c288ee7cf97e0631526ed2a2
25074 <jiyap91011@gmail.com>
2025-09-08 09:21:01 +0530

    Add files via upload

---
+++ jiya array1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{int i,a[5];
+printf("Enter 5 values");
+for(i=0; i<5; i++)
+scanf("%d",&a[i]);
+printf("Values are");
+for(i=0;i<5;i++)
+printf("%d",a[i]);
+
+
+
+
+
+
+
+
+}
---
+++ jiya array2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int i,a[10];
+printf("Enter 10 values");
+for(i=0; i<10; i++)
+scanf("%d",&a[i]);
+printf("4th value is %d\n",a[3]);
+printf("7th value is %d\n",a[6]);
+printf("9th value is %d\n",a[8]);
+
+
+
+}
